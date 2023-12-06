25278f8e4879630783fe4bd33fea224921b0f4e4
Daniel Imre Pethes <16624998+pethesdaniel@users.noreply.github.com>
2023-12-06 19:13:13 +0100

    Add project files.

---
+++ init.sql
@@ -0,0 +1,15 @@
+create table [NetflixTitles](
+[show_id] nvarchar(max),
+[type] nvarchar(max),
+[title] nvarchar(max),
+[director] nvarchar(max),
+[cast] nvarchar(max),
+[country] nvarchar(max),
+[date_added] datetime,
+[release_year] int,
+[rating] nvarchar(max),
+[duration] nvarchar(max),
+[listed_in] nvarchar(max),
+[description] nvarchar(max)
+)
+GO

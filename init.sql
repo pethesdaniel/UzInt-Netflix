49cb9e02423c02f2d18bd95993760bf8353a2c2a
Daniel Imre Pethes <16624998+pethesdaniel@users.noreply.github.com>
2023-12-07 05:17:19 +0100

    Ratings

--- init.sql
+++ init.sql
@@ -2,14 +2,28 @@ create table [NetflixTitles](
 [show_id] nvarchar(max),
 [type] nvarchar(max),
 [title] nvarchar(max),
-[director] nvarchar(max),
-[cast] nvarchar(max),
-[country] nvarchar(max),
 [date_added] datetime,
 [release_year] int,
-[rating] nvarchar(max),
-[duration] nvarchar(max),
 [listed_in] nvarchar(max),
-[description] nvarchar(max)
+)
+GO
+
+create table [NetflixStats](
+[year] int,
+[revenue] float,
+[subscribers] float,
+)
+GO
+
+create table [NetflixGenres](
+show_id nvarchar(max),
+genre nvarchar(max)
+)
+GO
+
+CREATE TABLE [dbo].[NetflixRatings](
+	[show_id] nvarchar(max),
+	[rating] float,
+	[num_of_votes] decimal,
 )
 GO
